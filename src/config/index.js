7554813c4a1ef5333ea95fb5c0fc691c9dc63015
Axel Setyanto <laxels@gmail.com>
2022-09-16 09:33:27 +0200

    point to prod backend

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,8 @@ const config = {
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   // config.BASE_URL = process.env.REACT_APP_API_URL;
-  config.BASE_URL = `http://localhost:1337`;
+  // config.BASE_URL = `http://localhost:1337`;
+  config.BASE_URL = `https://stackoverflow-clone-backend.herokuapp.com`;
 }
 
 export default config;
